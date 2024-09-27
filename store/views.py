e689b5760950f2df56c6edd85453a04e73713519
Chuks-ked <ikedichukwubuikem@gmail.com>
2024-09-27 06:00:35 +0100

    getting item

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.db.models.aggregates import Count 
+from django.db.models.aggregates import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
 from rest_framework.filters import SearchFilter, OrderingFilter
@@ -13,6 +13,7 @@ from .pagination import DefaultPagination
 
 # Create your views here.
 
+
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -22,39 +23,46 @@ class ProductViewSet(ModelViewSet):
     ordering_fields = ['unit_price', 'last_update', ]
     pagination_class = DefaultPagination
 
-
     def get_serializer_context(self):
         return {'request': self.request}
-    
+
     def destroy(self, request, *args, **kwargs):
         if OrderItem.object.filter(product_id=kwargs['pk'].count() > 0):
             return Response({'error': 'Product cannot be deleted because it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        
+
         return super().destroy(request, *args, **kwargs)
 
 
-
 class CollectionViewSet(ModelViewSet):
-    queryset = Collection.objects.annotate(products_count=Count('products')).all()
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
     serializer_class = CollectionSerializer
 
     def delete(self, request, pk):
         collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0:
-            return Response({'error':'Collection cannot be deleted because it includes one or more products'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
-    
+
     def get_queryset(self):
         return Review.objects.filter(product_id=self.kwargs['product_pk'])
 
     def get_serializer_context(self):
         return {'product_id': self.kwargs['product_pk']}
 
+
 class CartViewset(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
     queryset = Cart.objects.prefetch_related('items__product').all()
     serializer_class = CartSerializer
+
+
+class CartItemViewSet(ModelViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart_id=self.kwargs['cart_pk']).select_related('product')
