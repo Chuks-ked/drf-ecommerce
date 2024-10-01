4d6c8068733c1d352a148e755ab093280034dede
Chuks-ked <ikedichukwubuikem@gmail.com>
2024-10-01 08:45:35 +0100

    profile api

--- store/serializers.py
+++ store/serializers.py
@@ -92,9 +92,10 @@ class UpdateCartItemSerializer(serializers.ModelSerializer):
         model = CartItem
         fields = ['quantity']
 
+
 class CustomerSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField()
+    
     class Meta:
         model = Customer
-        fields = ['id', 'user_id', 'phone', 'birth_date', 'membership']
-        
+        fields = ['id','user_id', 'phone', 'birth_date', 'membership']
